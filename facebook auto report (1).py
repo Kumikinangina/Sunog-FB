4851d973b36012f3f5047906b4e1d25c8da428fa
sharleyy <168252040+Kumikinangina@users.noreply.github.com>
2025-05-14 18:26:40 +0800

    Add files via upload

---
+++ facebook auto report (1).py
@@ -0,0 +1,114 @@
+class colors:
+    BIGreen = "\033[1;92m"
+    cyan = "\033[1;36m"
+    BWhite='\033[1;37m'
+    BPurple='\033[1;35m'
+    BRed='\033[1;31m'
+    On_IYellow='\033[0;103m'
+
+print(colors.BIGreen + """
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣶⣶⠖⠀⠀⠲⣶⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠋⠀⠀⠀⠀⠀⠀⠙⢿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣾⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣷⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣾⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣷⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣿⣿⣿⣇⣤⠶⠛⣛⣉⣙⡛⠛⢶⣄⣸⣿⣿⣿⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀\033[1;92m⢀⣀⣿⣿⣿⡟⢁\033[1;31m⣴⣿⣿⣿⣿⣿⣿⣦\033[1;92m⡈⢿⣿⣿⣿⣀⡀\033[1;92m⠀⠀⠀⠀
+⠀⠀\033[1;92m⢠⣴⣿⣿⣿⣿⡟\033[1;31m⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\033[1;92m⡌⢿⣿⣿⣿⣿⣦⡄⠀\033[1;92m⠀
+⠀\033[1;92m⣴⣿⣿⡿⠿⢛⣻⡇\033[1;31m⢸⡟⠻⣿⣿⣿⣿⣿⡿⠟⢻⡇\033[1;92m⣸⣛⡛⠿⣿⣿⣿⣦\033[1;92m⠀
+\033[1;92m⢸⣿⡿⠋⠀⠀⢸⣿⣿\033[1;31m⡜⢧⣄⣀⣉⡿⣿⣉⣀⣠⣼\033[1;92m⢁⣿⣿⡇⠀⠀⠙⢿⣿⡆\033[1;92m
+\033[1;92m⣿⣿⠁⠀⠀⠀⠈⣿⣿\033[1;31m⡇⣿⡿⠛⣿⣵⣮⣿⡟⢻⡿\033[1;92m⢨⣿⣿⠀⠀⠀⠀⠈⣿⣿\033[1;92m
+\033[1;92m⢿⡟⠀⠀⠀⠀⠀⠘⣿⣷⣤⣄⡀\033[1;31m⣿⣿⣿⣿\033[1;92m⢁⣤⣶⣿⣿⠃⠀⠀⠀⠀⠀⣿⡟\033[1;92m
+\033[1;92m⠘⠇⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⡇\033[1;31m⢿⣿⣿⣿\033[1;92m⢸⣿⣿⠟⠁⠀⠀⠀⠀⠀ ⠻⠃\033[1;92m
+⠀⠀\033[1;92m⢀⡀⠀⠀⠀⠀⠀⠀⠀⢩⣦⣘⡘⠋⣛⣸⡍⠁⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀
+⠀⠀\033[1;92m⠘⢿⣷⣤⣤⣄⣤⣤⣶⣿⣿⣿⡿⢿⣿⣿⣿⣷⣤⣤⣠⣤⣴⣾⡿⠁\033[1;92m⠀⠀
+⠀⠀⠀⠀\033[1;92m⠉⠛⠿⠿⠿⡿⠿⠿⠛⠉⠀⠀⠉⠛⠿⠿⣿⠿⠿⠿⠛⠉\033[1;92m⠀⠀⠀⠀
+\033[1;37m╔═══════════════════════════════════╗\033[1;37m
+\033[1;37m║   \033[1;36m╔═╗╔═╗╔═╗╔═╗╔╗ ╔═╗╔═╗╦╔═        \033[1;37m║
+\033[1;37m║   \033[1;36m╠╣ ╠═╣║  ║╣ ╠╩╗║ ║║ ║╠╩╗        \033[1;37m║
+\033[1;37m║  \033[1;37m ╚  ╩ ╩╚═╝╚═╝╚═╝╚═╝╚═╝╩ ╩        \033[1;37m║
+\033[1;37m║   \033[1;37m╦═╗╔═╗╔═╗╔═╗╦═╗╔╦╗              \033[1;37m║
+\033[1;37m║   \033[1;35m╠╦╝║╣ ╠═╝║ ║╠╦╝ ║               \033[1;37m║
+\033[1;37m║   \033[1;35m╩╚═╚═╝╩  ╚═╝╩╚═ ╩ v1            \033[1;37m║
+\033[1;37m╚═══════════════════════════════════╝\033[1;31m
+\033[1;37m╔══════════════════════════════════════════════════╗
+\033[1;37m║\033[1;31mCREATED BY : MR.DARK HCKTVST                      \033[1;37m║
+\033[1;37m║\033[1;31mCREATED-TIME :- Monday, Dec 13, 2024         \033[1;37m     ║
+\033[1;37m║\033[1;31mGITHUB :- https://github.com/Darkness1268         \033[1;37m║
+\033[1;37m║\033[1;31mFACEBOOK :- https://www.facebook.com/MrDarkHcktvst\033[1;37m║
+\033[1;37m╚══════════════════════════════════════════════════╝
+\033[1;92m
+""")
+
+# Facebook Reporting Tool
+
+import requests  
+import json  
+
+class FacebookAccountReporter:  
+    def __init__(self, access_token):  
+        self.access_token = access_token  
+        self.base_url = "https://graph.facebook.com/v13.0/me"  
+
+    def get_account_data(self):  
+        params = {  
+            "access_token": self.access_token,  
+            "fields": "id,name,email,birthday,location"  
+        }  
+        response = requests.get(self.base_url, params=params)  
+        return response.json()  
+
+    def get_friends(self):  
+        params = {  
+            "access_token": self.access_token,  
+            "fields": "friends"  
+        }  
+        response = requests.get(self.base_url, params=params)  
+        return response.json()  
+
+    def get_posts(self):  
+        params = {  
+            "access_token": self.access_token,  
+            "fields": "posts"  
+        }  
+        response = requests.get(self.base_url, params=params)  
+        return response.json()  
+
+    def generate_report(self):  
+        account_data = self.get_account_data()  
+        friends = self.get_friends()  
+        posts = self.get_posts()  
+
+        report = {  
+            "Account Data": account_data,  
+            "Friends": friends,  
+            "Posts": posts  
+        }  
+
+        with open("facebook_account_report.json", "w") as f:  
+            json.dump(report, f, indent=4)  
+
+# Usage  
+if __name__ == "__main__":
+    access_token = input("Please enter your Facebook access token: ")  
+    reporter = FacebookAccountReporter(access_token)  
+    reporter.generate_report()
+
+
+def get_facebook_post(uid, post_id):
+    access_token = 'YOUR_ACCESS_TOKEN'
+    url = f'https://graph.facebook.com/v12.0/{post_id}?fields=message,created_time,from&access_token={access_token}'
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        post_data = response.json()
+        print(f"Post from: {post_data['from']['name']}")
+        print(f"Message: {post_data.get('message', 'No message available')}")
+        print(f"Created Time: {post_data['created_time']}")
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+
+if __name__ == "__main__":
+    uid = input("Enter Facebook UID: ")
+    post_id = input("Enter Facebook Post ID: ")
+    get_facebook_post(uid, post_id)
